69ef4b9c6f3a15d84b508a83dc8a0d47831d40ba
Светлана <svardakov@yandex.ru>
2020-03-29 14:16:41 +0500

    second commit

--- input_output.py
+++ input_output.py
@@ -5,4 +5,15 @@
 '''
 
 print('Hello')
-print('Hello world')
+print('Hello','Students',123)
+print('Hello','Students',123,sep='xxx')
+print('Hello','Students',123,sep='_',end='!')
+print() # чтобы следующая команда выполнялась с другой строки
+
+# Ввод
+
+name = input('input your name')
+print(name, type(name))
+age = input('input your age')
+print(int(age), type(int(age))) # операция приведения типов
+
